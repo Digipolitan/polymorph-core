79ee3f6f5852e450a5c7ec551ec4eb9fa0432f35
Benoit BRIATTE <briatte.benoit@digipolitan.com>
2017-11-14 10:10:10 +0100

    update transformer names

--- Sources/PolymorphCore/TransformerFactory.swift
+++ Sources/PolymorphCore/TransformerFactory.swift
@@ -11,13 +11,14 @@ internal class TransformerFactory {
 
     public static func create(project: Project) -> [UUID: Transformer] {
         let transformers = [
-            Transformer(name: "timestamp"),
-            Transformer(name: "date", options: [
+            Transformer(name: "Timestamp"),
+            Transformer(name: "Date", options: [
                 .init(name: "format", required: true, value: "yyyy-MM-dd'T'HH:mm:ss.SSSZ")
             ]),
-            Transformer(name: "url", options: [
+            Transformer(name: "URL", options: [
                 .init(name: "encode", required: true, value: "false")
-            ])
+            ]),
+            Transformer(name: "Multilingual")
         ]
         var idx = 1
         var result = [UUID: Transformer]()
